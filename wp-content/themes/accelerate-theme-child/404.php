faebe39a359d1d7c965ed6d453614b9c6f75563d
mhiggins0228 <mhiggins0228@gmail.com>
2016-08-23 10:04:58 -0500

    lots of new files 082316

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found)
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+
+			<header class="page-header">
+				<h1 class="page-title"><?php _e( 'Not Found', '404' ); ?></h1>
+			</header>
+
+			<div class="page-wrapper">
+				<div class="page-content">
+					<h2><?php _e( 'What is your major malfunction?!?!', '404' ); ?></h2>
+					<p><?php _e( 'I order you to search!', '404' ); ?></p>
+					
+					<?php get_search_form(); ?>
+				</div><!-- .page-content -->
+			</div><!-- .page-wrapper -->
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * The template for displaying the About page
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+			<?php while ( have_posts() ) : the_post(); 
+			 	$services_1_image = get_field ('services_1_image');
+				$services_2_image = get_field ('services_2_image');
+				$services_3_image = get_field ('services_3_image');
+				$services_4_image = get_field ('services_4_image');
+				
+				?> 
+			
+			<article class = "case-study">
+				<aside class = "case-study-sidebar">
+				<h2><?php the_title(); ?></h2> 	
+				<h5><?php echo $services; ?></h5>
+	
+							
+				<?php the_content(); ?>
+				
+				<p><a href ="<?php echo $link; ?>">Site Link</a></p> 
+				</aside>
+				
+				<div class="about-images">
+					<img src="<?php echo $services_1_image; ?>" />
+					<img src="<?php echo $services_2_image; ?>" />
+					<img src="<?php echo $services_3_image; ?>" />
+					<img src="<?php echo $services_4_image; ?>" />
+				
+				</div>			
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
